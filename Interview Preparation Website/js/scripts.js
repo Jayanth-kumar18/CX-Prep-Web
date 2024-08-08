da9296c41ec5daf8ef20b9a5b2329d48a60fdda2
jayanth-kumar18 <jayanth18jay@gmail.com>
2024-08-08 18:48:35 +0530

    first commit

---
+++ Interview Preparation Website/js/scripts.js
@@ -0,0 +1,60 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+function navigateToSection(portfolio) {
+    document.getElementById(portfolio).scrollIntoView({ behavior: 'smooth' });
+}
+function navigateToSection(about1) {
+    document.getElementById(about1).scrollIntoView({ behavior: 'smooth' });
+}
+function navigateToSection(team) {
+    document.getElementById(team).scrollIntoView({ behavior: 'smooth' });
+}
+document.querySelectorAll('.question').forEach(question => {
+    question.addEventListener('click', () => {
+        question.parentElement.classList.toggle('active');
+    });
+});
